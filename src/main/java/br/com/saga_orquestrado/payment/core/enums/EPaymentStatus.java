0174487c512fa481eb8ee38332f3afe795ca1415
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-10-28 21:44:27 -0300

    Creating modal class and repository

---
+++ src/main/java/br/com/saga_orquestrado/payment/core/enums/EPaymentStatus.java
@@ -0,0 +1,7 @@
+package br.com.saga_orquestrado.payment.core.enums;
+
+public enum EPaymentStatus {
+    PENDING,
+    SUCCESS,
+    REFUND;
+}
---
+++ src/main/java/br/com/saga_orquestrado/payment/core/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package br.com.saga_orquestrado.payment.core.repository;
+
+import br.com.saga_orquestrado.payment.core.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+
+    boolean existsByOrderIdAndTransactionId(String orderId, String transactionId);
+    Optional<Payment> findByOrderIdAndTransactionId(String orderId, String transactionId);
+}
